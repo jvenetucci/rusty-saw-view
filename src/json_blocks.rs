e92b1579209ff1c60e98d0a1fd507efb60da2a72
Joseph Venetucci <venetuc@pdx.edu>
2018-08-14 13:39:18 -0700

    Added basic display function for BlockData

--- src/json_blocks.rs
+++ src/json_blocks.rs
@@ -18,38 +18,59 @@ impl BlockData {
         for block in self.data.iter() {
             // Only print out the first block if full_id is true
             if show_genesis || block.header.block_num != "0" {
-                println!("{}{}", "Block ".green().bold().on_black(), block.header.block_num.green().bold().on_black());
+                println!("{}{}{}", "|Block ".green().bold().on_black(), block.header.block_num.green().bold().on_black(), " ".on_black());
                 if full_id {
-                    println!("  ID: {}", block.header_signature.magenta());
-                    println!("  Signer Pub Key: {}", block.header.signer_public_key);
+                    println!("| ID: {}", block.header_signature.magenta());
+                    println!("| Previous Block ID: {}", block.header.previous_block_id.magenta());
+                    println!("| Signer Pub Key: {}", block.header.signer_public_key);
                 } else {
-                    println!("  ID: {}...{}", &block.header_signature[0..6].magenta(), &block.header_signature[0..4].magenta());
-                    println!("  Signer Pub Key: {}..{}", &block.header.signer_public_key[0..6], &block.header.signer_public_key[0..4]);
+                    println!("| ID: {}...{}", &block.header_signature[0..6].magenta(), &block.header_signature[0..4].magenta());
+                    println!("| Previous Block ID: {}...{}", &block.header.previous_block_id[0..6].magenta(), &block.header.previous_block_id[0..4].magenta());
+                    println!("| Signer Pub Key: {}...{}", &block.header.signer_public_key[0..6], &block.header.signer_public_key[0..4]);
                 }
 
-            }
-        //     if block.header.block_num != "0" { //Skip the genesis/settings block
-        //         for batch in block.batches.iter() {
-        //             for txn in batch.transactions.iter() {
-        //                 // println!("\tData as Base64: {:x?}", txn.payload.as_str());
+                match block.get_num_batches() {
+                    1 => println!("| There is 1 batch in this block"),
+                    count => println!("| There are {} batches in this block", count),
+                }
 
-        //                 let bytes = base64::decode(txn.payload.as_str()).unwrap();
-        //                 // println!("\tData as Byte Array: {:x?}", bytes);
+                for (count, batch) in block.batches.iter().enumerate() {
+                    println!("\t{}{}{}", "|Batch ".green().bold().on_black(), count.to_string().green().bold().on_black(), " ".on_black());
+                    if full_id {
+                        println!("\t| ID: {}", batch.header_signature);
+                        println!("\t| Signer Pub Key: {}", batch.header.signer_public_key);
+                    } else {
+                        println!("\t| ID: {}...{}", &batch.header_signature[0..6], &batch.header_signature[0..4]);
+                        println!("\t| Signer Pub Key: {}...{}", &batch.header.signer_public_key[0..6], &batch.header.signer_public_key[0..4]);
+                    }
 
-        //                 let va: serde_cbor::Value = serde_cbor::from_slice(&bytes).unwrap();
-        //                 let pq = va.as_object().unwrap();
+                    match batch.get_num_txns() {
+                    1 => println!("\t| There is 1 transaction in this batch"),
+                    count => println!("\t| There are {} transactions in this batch", count),
+                    }
 
-        //                 for (key, val) in pq.iter() {
-        //                     println!("\t\t{:?} : {:?}", key, val);
-        //                 }
-        //             }
-        //         }
+                    for (count, txn) in batch.transactions.iter().enumerate() {
+                        println!("\t\t{}{}{}", "|Transaction ".green().bold().on_black(), count.to_string().green().bold().on_black(), " ".on_black());
+                        if full_id {
+                            println!("\t\t| ID: {}", txn.header_signature);
+                            println!("\t\t| Signer Pub Key: {}", txn.header.signer_public_key);
+                        } else {
+                            println!("\t\t| ID: {}...{}", &txn.header_signature[0..6], &txn.header_signature[0..4]);
+                            println!("\t\t| Signer Pub Key: {}...{}", &txn.header.signer_public_key[0..6], &txn.header.signer_public_key[0..4]);
+                        }
+
+                        println!("\t\t|\tData: {}", txn.payload.blue());
+
+                    }
+                }
+                println!("{}", "\n\t\t| |\n\t\t| |\n\t\t\\ /\n\t\t V \n".green());
             }
         }
+    }
 }
 
 #[cfg(test)]
-mod test_blockdata {
+mod test_blockdata_struct {
     use super::*;
 
     #[test]
@@ -81,6 +102,38 @@ pub struct Block {
     header_signature: String
 }
 
+impl Block {
+    pub fn get_num_batches(&self) -> usize {
+        self.batches.len()
+    }
+}
+
+#[cfg(test)]
+mod test_block_struct {
+    use super::*;
+
+    #[test]
+    fn empty_has_zero_batches() {
+        let data = Block::default();
+        assert_eq!(0, data.get_num_batches());
+    }
+
+    #[test]
+    fn one_has_one_batch() {
+        let mut data = Block::default();
+        data.batches.push(Batch::default());
+        assert_eq!(1, data.get_num_batches());
+    }
+
+    #[test]
+    fn two_has_two_batches() {
+        let mut data = Block::default();
+        data.batches.push(Batch::default());
+        data.batches.push(Batch::default());
+        assert_eq!(2, data.get_num_batches());
+    }
+}
+
 #[derive(Deserialize, Debug, Default)]
 pub struct BlockHeader {
     batch_ids: Vec<String>,
@@ -91,7 +144,7 @@ pub struct BlockHeader {
     state_root_hash: String
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Default)]
 pub struct Batch {
     header: BatchHeader,
     header_signature: String,
@@ -99,13 +152,45 @@ pub struct Batch {
     transactions: Vec<Transaction>,
 }
 
-#[derive(Deserialize, Debug)]
+impl Batch {
+    pub fn get_num_txns(&self) -> usize {
+        self.transactions.len()
+    }
+}
+
+#[cfg(test)]
+mod test_batch_struct {
+    use super::*;
+
+    #[test]
+    fn empty_has_zero_txns() {
+        let data = Batch::default();
+        assert_eq!(0, data.get_num_txns());
+    }
+
+    #[test]
+    fn one_has_one_txns() {
+        let mut data = Batch::default();
+        data.transactions.push(Transaction::default());
+        assert_eq!(1, data.get_num_txns());
+    }
+
+    #[test]
+    fn two_has_two_txns() {
+        let mut data = Batch::default();
+        data.transactions.push(Transaction::default());
+        data.transactions.push(Transaction::default());
+        assert_eq!(2, data.get_num_txns());
+    }
+}
+
+#[derive(Deserialize, Debug, Default)]
 pub struct BatchHeader {
     signer_public_key: String,
     transaction_ids: Vec<String>
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Default)]
 pub struct Transaction {
     header: TransactionHeader,
     header_signature: String,
@@ -118,7 +203,7 @@ pub struct Paging {
     start: Option<String>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Default)]
 pub struct TransactionHeader {
     batcher_public_key: String,
     dependencies: Vec<String>,
